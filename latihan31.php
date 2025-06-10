2ab8ff275c1ed7951e4ab3129e4bbf7159115d74
DheaPrecellia <2400018273@webmail.uad.ac.id>
2025-06-10 08:41:53 +0700

    Add files via upload

---
+++ latihan31.php
@@ -0,0 +1,19 @@
+<?php
+$a = 5;
+$b = 2;
+
+$hasiljumlah = $a + $b;
+echo $hasiljumlah;
+echo "<br><br>";
+
+$hasilkali = $a * $b;
+echo $hasilkali . "<br><br>";
+
+if ($a == $b) {
+    echo "a sama dengan b";
+} elseif ($a > $b) {
+    echo "a lebih besar dari pada b";
+} else {
+    echo "a lebih kecil dari pada b";
+}
+?>
